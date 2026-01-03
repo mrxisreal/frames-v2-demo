fcef548a1dc137abaa58e18f41fe1d3a12a552c8
Alireza-Asakri <mrx427173@gmail.com>
2026-01-04 01:29:20 +0330

    345

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,8 +4,8 @@ import { twMerge } from "tailwind-merge"
 export const METADATA = {
   name: "Space Drive",
   description: "Access your files from anywhere, anytime.",
-  bannerImageUrl: `${process.env.NEXT_PUBLIC_URL}/assets/screens/splash-02.png`,
-  iconImageUrl: 'https://frames-v2-demo-lilac.vercel.app/icon.png',
+  bannerImageUrl: `${process.env.NEXT_PUBLIC_URL}/assets/screens/splash-03.png`,
+  iconImageUrl: `${process.env.NEXT_PUBLIC_URL}/icon.png`,
   homeUrl: process.env.NEXT_PUBLIC_URL ?? "https://frames-v2-demo-lilac.vercel.app",
   // homeUrl: "https://frames-v2-demo-lilac.vercel.app",
   splashBackgroundColor: "#131313"
